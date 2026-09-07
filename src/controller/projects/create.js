7d47517b23f088801e12ad49202e119e360f3850
GTM35 <gustavotmsilva05@gmail.com>
2026-09-07 12:52:01 -0300

    :sparkles: feat: Implementando funcionalidade de criar projetos

--- src/controller/projects/create.js
+++ src/controller/projects/create.js
@@ -1,3 +1,15 @@
+import { randomInt } from "node:crypto";
+
 export function create(request, response) {
-  response.end("Projeto criado!");
+  const { name, description, owner } = request.body;
+
+  const project = {
+    id: randomInt(100),
+    name,
+    description,
+    owner,
+    create,
+  };
+
+  return response.end(JSON.stringify(project));
 }
